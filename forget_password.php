68d1c6c75daade61684d99cb179f26cfa592421e
Yusuf Kolayo <yussuf0019@gmail.com>
2023-10-02 14:26:29 +0100

    smtp credentials leak fix

--- forget_password.php
+++ forget_password.php
@@ -53,8 +53,8 @@ $mail = new PHPMailer(true);
                                 $mail->isSMTP();                                            //Send using SMTP
                                 $mail->Host       = 'server273.web-hosting.com';                     //Set the SMTP server to send through
                                 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                                $mail->Username   = 'fprochat@qatru.com';                     //SMTP username
-                                $mail->Password   = 'OUP-Ia{G]C7U';                               //SMTP password
+                                $mail->Username   = '';                     //SMTP username
+                                $mail->Password   = '';                               //SMTP password
                                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
                                 $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
                             
